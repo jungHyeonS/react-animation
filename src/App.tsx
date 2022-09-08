f7f20c17e36e7cf71f425277bb7cf93eb58c065b
jungHyeonS <fbtls25@naver.com>
2022-09-08 22:25:13 +0900

    react-master-class : variants

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,60 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background: white;
+  background: rgba(255,255,255,0.2);
   border-radius: 15px;
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
   box-shadow: 0 2px 3p rgba(0,0,0,0.1) , 0 10px 20px rgba(0,0,0,0.06);
 `
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  border-radius: 35px;
+  place-self: center;
+  box-shadow: 0 2px 3p rgba(0,0,0,0.1) , 0 10px 20px rgba(0,0,0,0.06);
+`
+
+const boxVariants = {
+  start : {
+    opacity : 0,
+    scale:0.5
+  },
+  end : {
+    opacity : 1,
+    scale:1,
+    transition : {
+      type : "spring",
+      duration : 0.5,
+      bounce : 0.5,
+      delayChildren: 0.3,
+      staggerChildren: 0.2
+    }
+  }
+}
+
+const circleVariants = {
+  start : {
+    opacity : 0,
+    y:10
+  },
+  end : {
+    opacity : 1,
+    y:0,
+  }
+}
 
 function App() {
   return (
     <Wrapper>
-      <Box transition={{type:"spring"}} initial={{scale:0}} animate={{scale:1,rotateZ:360}}/>
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle  variants={circleVariants}></Circle>
+        <Circle variants={circleVariants}></Circle>
+        <Circle variants={circleVariants}></Circle>
+        <Circle variants={circleVariants}></Circle>
+      </Box>
     </Wrapper>
   );
 }
